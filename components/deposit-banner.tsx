49deefde6cfc70b96c404a274c4c844374c126b5
Grant7117 <grantwhitburn@gmail.com>
2026-04-15 19:43:17 +0200

    feat: deploy optimized 60% content banners for Pricing, BetterBond, Partners, and Deposit

--- components/deposit-banner.tsx
+++ components/deposit-banner.tsx
@@ -3,7 +3,7 @@ import { StandardBanner } from "./standard-banner"
 export function DepositBanner() {
   return (
     <StandardBanner
-      src="/images/R5000%20deposit%20.png"
+      src="/images/R5000%20deposit.png"
       alt="R5000 Deposit Banner"
     />
   )
